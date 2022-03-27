454e03e0417e34d3b14b3f4f5d183a23b3741e7b
wudueuk <wudueuk@gmail.com>
2022-03-27 16:24:04 +0200

    first commit

---
+++ credit-card-2lvl/js/app.js
@@ -0,0 +1,85 @@
+import {
+  el,
+  setChildren
+} from '../node_modules/redom/dist/redom.es.js';
+import creditcard from './modules/creditcard.js';
+import form from './modules/form.js';
+import mask from './modules/mask.js';
+
+const pageTitle = el('div.payment-title', el('h1', 'Payment Information'));
+const container = el('div.container');
+
+setChildren(container, creditcard);
+setChildren(document.body, [pageTitle, container, form]);
+
+const name = document.getElementById('name');
+const cardnumber = document.getElementById('cardnumber');
+const cardnumberMask = IMask(cardnumber, {
+  mask: '0000 0000 0000 0000',
+  lazy: true,
+  maxLength: 16,
+});
+//cardnumberMask.on('complete');
+const expirationdate = document.getElementById('expirationdate');
+const dateMask = IMask(expirationdate, {
+  mask: '00/00',
+});
+const securitycode = document.getElementById('securitycode');
+const ccicon = document.getElementById('ccicon');
+const svgname = document.getElementById('svgname');
+const svgnumber = document.getElementById('svgnumber');
+const svgnameback = document.getElementById('svgnameback');
+const svgexpire = document.getElementById('svgexpire');
+const svgsecurity = document.getElementById('svgsecurity');
+const lightcolor = document.querySelectorAll('.lightcolor');
+const darkcolor = document.querySelectorAll('.darkcolor');
+
+name.addEventListener('input', () => {
+  if (name.value !== '') {
+    svgname.textContent = name.value.toUpperCase();
+  } else svgname.textContent = 'YOUR NAME';
+  svgnameback.textContent = svgname.textContent;
+});
+
+expirationdate.addEventListener('input', () => {
+  svgexpire.textContent = expirationdate.value;
+});
+
+securitycode.addEventListener('input', () => {
+  svgsecurity.textContent = securitycode.value;
+});
+
+const getCardOwner = value => {
+  return mask.find(elem => value.match(elem.regex) ? elem : undefined);
+};
+
+export const swapColor = color => {
+  lightcolor.forEach(elem => {
+    elem.classList.remove('grey');
+    elem.classList.add(color);
+  });
+  darkcolor.forEach(elem => {
+    elem.classList.remove('greydark');
+    elem.classList.add(color + 'dark');
+  });
+};
+
+cardnumber.addEventListener('input', () => {
+  const value = cardnumber.value;
+  const cardOwner = getCardOwner(value);
+  if (cardOwner.cardType !== '' && cardOwner.cardType !== 'Unknown') {
+    ccicon.innerHTML = cardOwner.icon;
+    cardnumberMask.updateOptions({
+      mask: cardOwner.mask,
+      maxLength: cardOwner.mask.length + 2,
+    });
+    swapColor(cardOwner.color);
+  } else {
+    swapColor('grey');
+    cardnumberMask.updateOptions({
+      mask: '0000 0000 0000 0000',
+    });
+    ccicon.innerHTML = '';
+  }
+  svgnumber.textContent = value;
+});
---
+++ credit_card-1lvl/js/app.js
@@ -0,0 +1,107 @@
+﻿import { el, setChildren } from '../node_modules/redom/dist/redom.es.js';
+import CreditCardSpace from './credit-card-space.js';
+
+const wrapper = el('div.wrapper');
+
+const card = el('div.card');
+const cardNumber = el('span.card__number', 'xxxx xxxx xxxx xxxx');
+const cardName = el('span.card__name', 'YOUR NAME');
+const cardDate = el('span.card__date', 'DD/YY');
+
+const form = el('form.form', {
+  action: '#',
+  id: 'form',
+});
+form.addEventListener('submit', e => {
+  e.preventDefault();
+})
+
+const inputHolder = el('input.input.input__holder', {
+  type: 'text',
+  id: 'inputHolder',
+});
+inputHolder.addEventListener('input', () => {
+  if (inputHolder.value !== '') {
+    cardName.textContent = inputHolder.value.toUpperCase();
+  } else cardName.textContent = 'YOUR NAME';
+});
+
+const inputNumber = el('input.input.input__number', {
+  type: 'text',
+  id: 'cardNumber',
+});
+new CreditCardSpace(inputNumber);
+inputNumber.addEventListener('input', () => {
+  cardNumber.textContent = inputNumber.value;
+});
+
+const inputDate = el('input.input.input__date', {
+  type: 'text',
+  id: 'inputeDate',
+});
+inputDate.addEventListener('blur', () => {
+  inputDate.value = inputDate.value.replace(
+    /^([1-9]\/|[2-9])$/g, '0$1/'
+  ).replace(
+    /^(0[1-9]|1[0-2])$/g, '$1/'
+  ).replace(
+    /^([0-1])([3-9])$/g, '0$1/$2'
+  ).replace(
+    /^(0?[1-9]|1[0-2])([0-9]{2})$/g, '$1/$2'
+  ).replace(
+    /^([0]+)\/|[0]+$/g, '0'
+  ).replace(
+    /[^\d\/]|^[\/]*$/g, ''
+  ).replace(
+    /\/\//g, '/'
+  );
+  cardDate.textContent = inputDate.value;
+});
+
+const inputCvv = el('input.input.input__cvv', {
+  type: 'text',
+  id: 'inputCvv',
+});
+
+setChildren(form, [
+  el('div.form__input-wrap.form__input-wrap_holder', [
+    el('label.form__label.form__holder-label', 'Card Holder', {
+      for: 'inputHolder',
+    }),
+    inputHolder
+  ]),
+  el('div.form__input-wrap.form__input-wrap_number', [
+    el('label.form__label.form__number-label', 'Card Number', {
+      for: 'cardNumber',
+    }),
+    inputNumber
+  ]),
+  el('div.form__input-wrap.form__input-wrap_date', [
+    el('label.form__label.form__date-label', 'Card Expiry', {
+      for: 'inputDate',
+    }),
+    inputDate
+  ]),
+  el('div.form__input-wrap.form__input-wrap_cvv', [
+    el('label.form__label.form__cvv-label', 'CVV', {
+      for: 'inputCvv',
+    }),
+    inputCvv
+  ]),
+  el('button.form__button', 'CHECK OUT')
+]);
+
+setChildren(card, [
+  el('p.secure', 'Secure Checkout'),
+  el('div.credit-card', [
+    cardNumber,
+    el('div.card__personal', [
+      cardName,
+      cardDate
+    ])
+  ]),
+  form
+]);
+
+setChildren(wrapper, card);
+setChildren(document.body, wrapper);
